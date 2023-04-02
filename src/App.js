d4c2b6faaa8017bdbfd5028b3f6b817f7948af71
harshlimbani <limbaniharsh1@gmail.com>
2023-04-02 22:43:38 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-import { Box, Button, FormControl, FormHelperText, FormLabel, Input, InputGroup, InputRightElement } from "@chakra-ui/react"
+import { Box, Button, ButtonGroup, Flex, FormControl, FormHelperText, FormLabel, Heading, Input, InputGroup, InputRightElement, Spacer } from "@chakra-ui/react"
 import React from 'react';
 
 function App() {
   return (
-    <div className='App-header'>
+    <div>
+      <Flex minWidth='max-content' p={5} alignItems='center' gap='2' backgroundColor='#282c34'>
+        <Box p='2'>
+          <Heading size='md' color='white'>Chakra App</Heading>
+        </Box>
+        <Spacer />
+        <ButtonGroup gap='2'>
+          <Button colorScheme='teal'>Sign Up</Button>
+          <Button colorScheme='teal'>Log in</Button>
+        </ButtonGroup>
+      </Flex>
+      <div className='App-header'>
       <Box width='500px' borderWidth="1px" borderRadius="lg" p={4}>
-      <FormControl>
-        <FormLabel>Name</FormLabel>
-        <Input type="text" />
-      </FormControl>
-      <FormControl mt={4}>
-        <FormLabel>Email</FormLabel>
-        <Input type="email" />
-      </FormControl>
-      <FormControl mt={4}>
-        <FormLabel>Password</FormLabel>
-        <Input type="password" />
-      </FormControl>
-      <Button mt={4} colorScheme="teal">Sign Up</Button>
-    </Box>
+        <FormControl>
+          <FormLabel>Name</FormLabel>
+          <Input type="text" />
+        </FormControl>
+        <FormControl mt={4}>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" />
+        </FormControl>
+        <FormControl mt={4}>
+          <FormLabel>Password</FormLabel>
+          <Input type="password" />
+        </FormControl>
+        <Button mt={4} colorScheme="teal">Sign Up</Button>
+      </Box>
+      </div>
 
     </div>
   );
