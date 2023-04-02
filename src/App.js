0f47b49eafd5a71a7f573018fbf8f7fade17e18b
harshlimbani <limbaniharsh1@gmail.com>
2023-04-02 22:26:44 +0530

    sign-up

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import { Box, Button, FormControl, FormHelperText, FormLabel, Input, InputGroup, InputRightElement } from "@chakra-ui/react"
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <Box maxW="sm" borderWidth="1px" borderRadius="lg" p={4}>
+      <FormControl>
+        <FormLabel>Name</FormLabel>
+        <Input type="text" />
+      </FormControl>
+      <FormControl mt={4}>
+        <FormLabel>Email</FormLabel>
+        <Input type="email" />
+      </FormControl>
+      <FormControl mt={4}>
+        <FormLabel>Password</FormLabel>
+        <Input type="password" />
+      </FormControl>
+      <Button mt={4} colorScheme="teal">Sign Up</Button>
+    </Box>
+
+    </div>
+  );
+}
+
+
+export default App;
